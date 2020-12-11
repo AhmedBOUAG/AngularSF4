53b0d497d3b27affaaf7f1749a39f6472b71ce7f
Aline <aline.rambier@gfi.fr>
2020-12-11 23:13:55 +0100

    Mise à jour sur routes et redirections

--- duFaitMaison/src/app/app-routing.module.ts
+++ duFaitMaison/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { RecetteComponent } from './recette/recette.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RecetteComponent } from './recette/recette.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'recette', component: RecetteComponent },
-  { path: 'home', component: HomeComponent}
+ 
 ];
 
 @NgModule({
