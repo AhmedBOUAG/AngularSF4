3fb9634f2ee4e93b81c84dc99e73cba2ada5bce4
Ahmed <ahmed.bouagoune@gmail.com>
2020-12-11 19:30:06 +0100

    premiere phase de développement

--- duFaitMaison/src/app/app-routing.module.ts
+++ duFaitMaison/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { RecetteComponent } from './recette/recette.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recette', component: RecetteComponent },
+  { path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
