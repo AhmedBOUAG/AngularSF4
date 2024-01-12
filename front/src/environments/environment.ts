25ab7b9f91dbcc729b956e3152d1fb1e813676af
Afkir Atman <atman.afkir@gfi.fr>
2024-01-12 21:46:25 +0100

    [Front] Gestion de la possibilité de définir un port par défaut dynamique pour le serveur web Symfony pour les appels api + [Core] Factorisation de code et fix

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -2,9 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { ports } from './ports';
+
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://127.0.0.1/',
+  apiBaseUrl: 'http://127.0.0.1' + ports.apiDefault + '/',
 };
 
 /*
